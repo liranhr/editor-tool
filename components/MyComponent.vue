691006060b2b3d0700e1166cb48fcebe03868206
Liran Hersh <liran.hersh@flex.com>
2024-06-27 14:52:36 +0300

    New Package

---
+++ components/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- Component HTML goes here -->
+    <p>This is my Vue.js component!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  // Component logic here
+};
+</script>
+
+<style scoped>
+
+</style>
