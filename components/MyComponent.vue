7877d3126bd2e070fb0aba46d02931306b2252bd
Liran Hersh <liran.hersh@flex.com>
2024-06-27 15:27:20 +0300

    textarea

--- components/MyComponent.vue
+++ components/MyComponent.vue
@@ -1,17 +1,42 @@
 <template>
   <div>
-    <!-- Component HTML goes here -->
-    <p>This is my Vue.js component!</p>
+    <div>
+        <button @click="toggleBold" class="bold-btn-style">B</button>
+        <button>/</button>
+        <button @click="toggleUnderline" class="underline-btn-style">U</button>
+    </div>
+    
+    <textarea name="" id="" cols="30" rows="10"></textarea>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MyComponent',
-  // Component logic here
+    data() {
+        return {
+            bold: false,
+            underline: false
+        };
+    },
+    methods:{
+        toggleBold(){
+            this.bold = !this.bold;
+            console.log(this.bold)
+        },
+        toggleUnderline(){
+            this.underline = !this.underline;
+        }
+    
+    }
 };
 </script>
 
 <style scoped>
-
+    .bold-btn-style {
+        font-weight: bold;
+    }
+    .underline-btn-style {
+        text-decoration: underline;
+    }
 </style>
