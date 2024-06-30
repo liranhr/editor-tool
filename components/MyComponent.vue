dd1970fee3dc84b6a4c8e7875399f5c41b8ea924
Liran Hersh <liran.hersh@flex.com>
2024-06-30 09:22:23 +0300

    color option

--- components/MyComponent.vue
+++ components/MyComponent.vue
@@ -1,42 +1,45 @@
 <template>
   <div>
     <div>
-        <button @click="toggleBold" class="bold-btn-style">B</button>
-        <button>/</button>
-        <button @click="toggleUnderline" class="underline-btn-style">U</button>
+      <button @click="toggleBold" class="bold-btn-style">B</button>
+      <button @click="toggleUnderline" class="underline-btn-style">U</button>
+      <!-- New button for changing text color -->
+      <button @click="changeTextColor('#ff0000')" class="color-btn-style">Color</button>
     </div>
-    
-    <textarea name="" id="" cols="30" rows="10"></textarea>
+    <div contenteditable="true" ref="textEditor" class="text-editor"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MyComponent',
-    data() {
-        return {
-            bold: false,
-            underline: false
-        };
+  methods: {
+    toggleBold() {
+      document.execCommand('bold', false, null);
     },
-    methods:{
-        toggleBold(){
-            this.bold = !this.bold;
-            console.log(this.bold)
-        },
-        toggleUnderline(){
-            this.underline = !this.underline;
-        }
-    
+    toggleUnderline() {
+      document.execCommand('underline', false, null);
+    },
+    // Method to change the text color
+    changeTextColor(color) {
+      document.execCommand('foreColor', false, color);
     }
+  }
 };
 </script>
 
-<style scoped>
-    .bold-btn-style {
-        font-weight: bold;
-    }
-    .underline-btn-style {
-        text-decoration: underline;
-    }
+<style>
+.text-editor {
+  border: 1px solid #ccc;
+  min-height: 100px;
+  margin-top: 10px;
+  padding: 5px;
+}
+.text-editor:focus {
+  outline: none;
+}
+/* Add your button styling here */
+.bold-btn-style, .underline-btn-style, .color-btn-style {
+  margin-right: 5px;
+}
 </style>
